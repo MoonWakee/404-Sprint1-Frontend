480b191ab5386337936cb121d169a8538cdd0d08
jainryu <jainryu0929@gmail.com>
2022-11-13 22:03:12 -0500

    login form

--- src/App.js
+++ src/App.js
@@ -5,32 +5,13 @@ import AddSchedulePage from './pages/addSchedulePage';
 import {BrowserRouter as Router, Routes , Route} from 'react-router-dom';
 import CreateGroup from './pages/addGroup';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 function App() {
   return(
     <Router>
         <Routes>
           <Route path = "/" element = {<SignUpForm/>} />
+          <Route path = "/Login" element = {<LoginForm/>} />
           <Route path="/AddSchedulePage" element = {<AddSchedulePage /> } />
           <Route path= "/CreateGroup" element = {<CreateGroup/>}/>
         </Routes>
---
+++ src/pages/login.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import Button from "./component/button";
+import { useNavigate } from "react-router-dom";
+
+
+const LoginForm = () => {
+    let navigate = useNavigate();
+
+    const [values, setValues] = useState({
+        username: "",
+        password: ""
+    });
+
+    const handleChange = (event) => {
+        setValues({
+            ...values,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        // navigate("/AddSchedulePage", { replace: true });
+        try {
+            let res = await fetch("http://127.0.0.1:5000/users/login", {
+                method: "POST",
+                // headers: {
+                //     "Content-Type": "application/json; charset=utf-8"
+                // },
+                body: JSON.stringify({
+                    username: values.username,
+                    password: values.password,
+                }),
+            });
+            let resJson = await res.json();
+            console.log(resJson)
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+
+    return (
+            <div>
+                <div>
+                    <h2 className="title"> Login </h2>
+                </div>
+                <form>
+                    <div>
+                        <label> Username </label>
+                        <input
+                            className="input"
+                            type="text"
+                            name="username"
+                            value={values.username}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div>
+                        <label> Password </label>
+                        <input
+                            className="input"
+                            type="text"
+                            name="password"
+                            value={values.password}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div>
+                        <Button handleClick={handleFormSubmit} buttonLabel="Login"></Button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+
+    export default LoginForm;
