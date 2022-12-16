bcf9c983a2ebf543b165b51f413f967385fb91fe
jainryu <jainryu0929@gmail.com>
2022-12-16 00:01:51 -0500

    login & signup

--- src/login.js
+++ src/login.js
@@ -21,9 +21,10 @@ const LoginForm = () => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
     // navigate("/AddSchedulePage", { replace: true });
+    console.log(values)
     try {
       let res = await fetch(
-        "http://ec2-44-206-245-116.compute-1.amazonaws.com:5000/users/login", 
+        "http://127.0.0.1:5000/api/user/login", 
         {
         method: "POST",
         body: JSON.stringify({
@@ -32,7 +33,7 @@ const LoginForm = () => {
         }),
       });
       let resJson = await res.json();
-      if (resJson.data.accessToken) {
+      if (resJson.data.token) {
         localStorage.setItem("user", JSON.stringify(resJson.data))
       }
       console.log(resJson);
@@ -43,63 +44,108 @@ const LoginForm = () => {
   };
 
   return (
-    <div class="grid place-items-center h-screen">
-      <div class="w-full max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <form class="space-y-6" action="#">
-          <h5 class="text-xl font-medium text-gray-900 dark:text-white">
-            Sign in to our WeMeet
-          </h5>
-          <div>
-            <label
-              for="email"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Your email
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-              placeholder="name@company.com"
-              required
-            />
-          </div>
-          <div>
-            <label
-              for="password"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Your password
-            </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="••••••••"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-              required
-            />
-          </div>
-          <button
-            onSubmit={handleFormSubmit}
-            type="submit"
-            class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Login to your account
-          </button>
-          <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-            Not registered?{" "}
-            <a
-              href="/login"
-              class="text-blue-700 hover:underline dark:text-blue-500"
-            >
-              Create account
-            </a>
-          </div>
-        </form>
+        <div className="px-64">
+      <div>
+        <h2 className="title"> Log In </h2>
       </div>
+      <form>
+        <div>
+          <InputLabel> Email </InputLabel>
+          <TextField
+            className="input"
+            type="text"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <InputLabel> Password </InputLabel>
+          <TextField
+            className="input"
+            type="text"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <Button
+            style={{ marginTop : 20 }}
+            variant="contained"
+            component="label"
+            color="primary"
+            onClick={handleFormSubmit}
+          >
+            Log In
+          </Button>
+        </div>
+      </form>
     </div>
+  );
+
+  // return (
+  //   <div class="grid place-items-center h-screen">
+  //     <div class="w-full max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+  //       <form class="space-y-6" action="#">
+  //         <h5 class="text-xl font-medium text-gray-900 dark:text-white">
+  //           Sign in to our WeMeet
+  //         </h5>
+  //         <div>
+  //           <label
+  //             for="email"
+  //             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+  //           >
+  //             Your email
+  //           </label>
+  //           <input
+  //             type="email"
+  //             name="email"
+  //             id="email"
+  //             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+  //             placeholder="name@company.com"
+  //             onChange = {handleChange}
+  //             required
+  //           />
+  //         </div>
+  //         <div>
+  //           <label
+  //             for="password"
+  //             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+  //           >
+  //             Your password
+  //           </label>
+  //           <input
+  //             type="password"
+  //             name="password"
+  //             id="password"
+  //             placeholder="••••••••"
+  //             onChange = {handleChange}
+  //             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+  //             required
+  //           />
+  //         </div>
+  //         <button
+  //           onSubmit={handleFormSubmit}
+  //           type="submit"
+  //           class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+  //         >
+  //           Login to your account
+  //         </button>
+  //         <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+  //           Not registered?{" "}
+  //           <a
+  //             href="/login"
+  //             class="text-blue-700 hover:underline dark:text-blue-500"
+  //           >
+  //             Create account
+  //           </a>
+  //         </div>
+  //       </form>
+  //     </div>
+  //   </div>
+
     // <div className="px-64">
     //   <div>
     //     <h2 className="title"> Log In </h2>
@@ -139,7 +185,7 @@ const LoginForm = () => {
     //     </div>
     //   </form>
     // </div>
-  );
+  // );
 };
 
 // export default LoginForm;
--- src/signup.js
+++ src/signup.js
@@ -26,7 +26,7 @@ const SignUpForm = () => {
 
     try {
       let res = await fetch(
-        "http://ec2-44-206-245-116.compute-1.amazonaws.com:5000/users/signup",
+        "http://127.0.0.1:5000/api/user/signup",
         {
           method: "POST",
           body: JSON.stringify({
@@ -39,7 +39,7 @@ const SignUpForm = () => {
         }
       );
       let resJson = await res.json();
-      if (resJson.data.accessToken) {
+      if (resJson.data.token) {
         localStorage.setItem("user", JSON.stringify(resJson.data))
       }
       console.log(resJson);
@@ -50,196 +50,194 @@ const SignUpForm = () => {
   };
 
   return (
-    <div class="grid place-items-center h-screen">
-      <div class="w-full max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <form class="space-y-6" action="#">
-          <h5 class="text-xl font-medium text-gray-900 dark:text-white">
-            Sign Up
-          </h5>
-          <div>
-            <label
-              for="email"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Your email
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-              placeholder="uni@columbi.edu"
-              value={values.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div class="grid gap-6 mb-6 md:grid-cols-2">
-            <div>
-              <label
-                for="first_name"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                First name
-              </label>
-              <input
-                type="text"
-                name="firstname"
-                id="firstname"
-                name="firstname"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                placeholder="Donald"
-                value={values.firstname}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <label
-                for="last_name"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Last name
-              </label>
-              <input
-                type="text"
-                name="lastname"
-                id="lastname"
-                name="lastname"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                placeholder="Ferguson"
-                value={values.lastname}
-                onChange={handleChange}
-                required
-              />
-            </div>
-          </div>
-          <div>
-            <label
-              for="email"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Your Username
-            </label>
-            <input
-              type="text"
-              name="username"
-              id="username"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-              placeholder="wemeet_love"
-              value={values.user_name}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div>
-            <label
-              for="password"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Your password
-            </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="••••••••"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-              value={values.password}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <button
-            onSubmit={handleFormSubmit}
-            component="label"
-            type="submit"
-            class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Sign Up
-          </button>
-        </form>
-      </div>
-    </div>
-    // <div className="px-64">
-    //   <div>
-    //     <h2 className="title"> Create Account </h2>
-    //   </div>
-    //   <form>
-    //     <div>
-    //       <InputLabel> Email </InputLabel>
-    //       <TextField
-    //         className="input"
-    //         type="text"
-    //         name="email"
-    //         value={values.email}
-    //         onChange={handleChange}
-    //       />
-    //     </div>
-    //     <div>
-    //       <Grid container style={{marginTop : 15}}>
-    //         <Grid item>
-    //           <InputLabel> First Name </InputLabel>
-    //           <TextField
-    //             className="input"
+    // <div class="grid place-items-center h-screen">
+    //   <div class="w-full max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+    //     <form class="space-y-6" action="#">
+    //       <h5 class="text-xl font-medium text-gray-900 dark:text-white">
+    //         Sign Up
+    //       </h5>
+    //       <div>
+    //         <label
+    //           for="email"
+    //           class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+    //         >
+    //           Your email
+    //         </label>
+    //         <input
+    //           type="email"
+    //           name="email"
+    //           id="email"
+    //           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+    //           placeholder="uni@columbi.edu"
+    //           value={values.email}
+    //           onChange={handleChange}
+    //           required
+    //         />
+    //       </div>
+    //       <div class="grid gap-6 mb-6 md:grid-cols-2">
+    //         <div>
+    //           <label
+    //             for="first_name"
+    //             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+    //           >
+    //             First name
+    //           </label>
+    //           <input
     //             type="text"
     //             name="firstname"
+    //             id="firstname"
+    //             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+    //             placeholder="Donald"
     //             value={values.firstname}
     //             onChange={handleChange}
+    //             required
     //           />
-    //         </Grid>
-    //         <br />
-    //         <Grid item style={{marginLeft : 30}}>
-    //           <InputLabel> Last Name </InputLabel>
-    //           <TextField
-    //             className="input"
+    //         </div>
+    //         <div>
+    //           <label
+    //             for="last_name"
+    //             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+    //           >
+    //             Last name
+    //           </label>
+    //           <input
     //             type="text"
     //             name="lastname"
+    //             id="lastname"
+    //             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+    //             placeholder="Ferguson"
     //             value={values.lastname}
     //             onChange={handleChange}
+    //             required
     //           />
-    //         </Grid>
-    //       </Grid>
-    //     </div>
-    //     <div>
-    //       <Grid container style={{marginTop : 15}}>
-    //         <Grid item>
-    //           <InputLabel> User Name </InputLabel>
-    //           <TextField
-    //             className="input"
-    //             type="text"
-    //             name="username"
-    //             value={values.user_name}
-    //             onChange={handleChange}
-    //           />
-    //         </Grid>
-    //         <Grid item style={{marginLeft : 30}}>
-    //           <InputLabel> Password </InputLabel>
-    //           <TextField
-    //             className="input"
-    //             type="text"
-    //             name="password"
-    //             value={values.password}
-    //             onChange={handleChange}
-    //           />
-    //         </Grid>
-    //       </Grid>
-    //     </div>
-    //     <div>
-    //       {/* <button
-    //                     className="submit" onClick = {handleFormSubmit}> Sign Up </button> */}
-    //       <Button
-    //         style={{ marginTop: 20 }}
-    //         variant="contained"
+    //         </div>
+    //       </div>
+    //       <div>
+    //         <label
+    //           for="email"
+    //           class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+    //         >
+    //           Your Username
+    //         </label>
+    //         <input
+    //           type="text"
+    //           name="username"
+    //           id="username"
+    //           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+    //           placeholder="wemeet_love"
+    //           value={values.user_name}
+    //           onChange={handleChange}
+    //           required
+    //         />
+    //       </div>
+    //       <div>
+    //         <label
+    //           for="password"
+    //           class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+    //         >
+    //           Your password
+    //         </label>
+    //         <input
+    //           type="password"
+    //           name="password"
+    //           id="password"
+    //           placeholder="••••••••"
+    //           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+    //           value={values.password}
+    //           onChange={handleChange}
+    //           required
+    //         />
+    //       </div>
+    //       <button
+    //         onSubmit={handleFormSubmit}
     //         component="label"
-    //         color="primary"
-    //         onClick={handleFormSubmit}
+    //         type="submit"
+    //         class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
     //       >
     //         Sign Up
-    //       </Button>
-    //     </div>
-    //   </form>
+    //       </button>
+    //     </form>
+    //   </div>
     // </div>
+    <div className="px-64">
+      <div>
+        <h2 className="title"> Create Account </h2>
+      </div>
+      <form>
+        <div>
+          <InputLabel> Email </InputLabel>
+          <TextField
+            className="input"
+            type="text"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <Grid container style={{marginTop : 15}}>
+            <Grid item>
+              <InputLabel> First Name </InputLabel>
+              <TextField
+                className="input"
+                type="text"
+                name="firstname"
+                value={values.firstname}
+                onChange={handleChange}
+              />
+            </Grid>
+            <br />
+            <Grid item style={{marginLeft : 30}}>
+              <InputLabel> Last Name </InputLabel>
+              <TextField
+                className="input"
+                type="text"
+                name="lastname"
+                value={values.lastname}
+                onChange={handleChange}
+              />
+            </Grid>
+          </Grid>
+        </div>
+        <div>
+          <Grid container style={{marginTop : 15}}>
+            <Grid item>
+              <InputLabel> User Name </InputLabel>
+              <TextField
+                className="input"
+                type="text"
+                name="username"
+                value={values.user_name}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item style={{marginLeft : 30}}>
+              <InputLabel> Password </InputLabel>
+              <TextField
+                className="input"
+                type="text"
+                name="password"
+                value={values.password}
+                onChange={handleChange}
+              />
+            </Grid>
+          </Grid>
+        </div>
+        <div>
+          {/* <button
+                        className="submit" onClick = {handleFormSubmit}> Sign Up </button> */}
+          <Button
+            style={{ marginTop: 20 }}
+            variant="contained"
+            component="label"
+            color="primary"
+            onClick={handleFormSubmit}
+          >
+            Sign Up
+          </Button>
+        </div>
+      </form>
+    </div>
   );
 };
 
