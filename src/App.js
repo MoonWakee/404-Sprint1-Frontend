ae819cd46dab37cbfbc180591fbf22cb8af2c987
Seong Moon Jo <sj3014@columbia.edu>
2022-12-16 02:40:27 -0500

    fixed and added codes for build

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import 'devextreme/dist/css/dx.common.css';
-import 'devextreme/dist/css/dx.light.css';
-import logo from './logo.svg';
-import './App.css';
-import SignUpForm from './signup';
-import AddSchedulePage from './pages/addSchedulePage';
-import { ListGroupPage } from './pages/listGroup';
-import { AddGroupPage } from './addGroup';
-import {BrowserRouter as Router, Routes , Route, useParams} from 'react-router-dom';
-import {LoginPage} from './login';
-import {SignUpPage} from './signup';
-import {CalendarPage} from './Calendar';
-import { EditGroupPage } from './pages/editGroup';
-
+import "devextreme/dist/css/dx.common.css";
+import "devextreme/dist/css/dx.light.css";
+import logo from "./logo.svg";
+import "./App.css";
+import SignUpForm from "./signup";
+import AddSchedulePage from "./pages/addSchedulePage";
+import { ListGroupPage } from "./pages/listGroup";
+import { AddGroupPage } from "./addGroup";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { LoginPage } from "./login";
+import { SignUpPage } from "./signup";
+import { CalendarPage } from "./Calendar";
+import { EditGroupPage } from "./pages/editGroup";
 
 function App() {
-  return(
+  return (
     <Router>
-        <Routes>
-          <Route path = "/" element = {<LoginPage/>} />
-          <Route path = "/calendar" element = {<CalendarPage/>} />
-          <Route path = "/signup" element = {<SignUpPage/>} />
-          <Route path="/AddSchedulePage" element = {<AddSchedulePage /> } />
-          <Route path="/AddGroupPage" element = {<AddGroupPage /> } />
-          <Route path="/listGroup" element = {<ListGroupPage /> } />
-          <Route path="/group/:groupid" element = {<EditGroupPage/> } />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/calendar" element={<CalendarPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/AddSchedulePage" element={<AddSchedulePage />} />
+        <Route path="/AddGroupPage" element={<AddGroupPage />} />
+        <Route path="/listGroup" element={<ListGroupPage />} />
+        <Route path="/group/:groupid" element={<EditGroupPage />} />
+      </Routes>
     </Router>
   );
 }
