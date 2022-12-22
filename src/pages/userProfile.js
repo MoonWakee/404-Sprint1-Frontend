5d64aad096d3dd8dd2c89f90ac87f3bc57235060
jainryu <jainryu0929@gmail.com>
2022-12-22 00:03:38 -0500

    user profile

--- src/pages/userProfile.js
+++ src/pages/userProfile.js
@@ -30,11 +30,27 @@ const UserProfile = () => {
 
 
   return (
+
     <div>
-        {profile.email}
+        {/* {profile.email}
         {profile.user_name}
         {profile.first_name}
-        {profile.last_name}
+        {profile.last_name} */}
+      <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+        {/* <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          UserName: {profile.user_name}
+        </p> */}
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> {profile.user_name} </h5>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          Email: {profile.email}
+        </p>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          First Name: {profile.first_name}
+        </p>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          Last Name: {profile.last_name}
+        </p>
+      </div>
     </div>
     
   );
