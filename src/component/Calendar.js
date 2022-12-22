3dea4c31d35edf713df413b9a4513830dd949027
Harry <jl5271@columbia.edu>
2022-12-22 00:15:11 -0500

    add schedule

--- src/component/Calendar.js
+++ src/component/Calendar.js
@@ -59,12 +59,13 @@ const fetchSchedule = async () => {
     });
     let data = await res.json();
     console.log(data);
+    return data;
   } catch (err) {
     console.error(err);
   }
 };
 
-const getData = async () => {
+const getData = () => {
 }
 
 const onAppointmentAdding = (e) => {
@@ -82,18 +83,21 @@ const onAppointmentAdding = (e) => {
   const end_time = date + " " + time;
 
   var description = JSON.stringify(e.appointmentData.description);
-  description = description.substring(1, description.length - 1);
+  if (description == undefined) description = null;
+  else description = description.substring(1, description.length - 1);
   var schedule_name = JSON.stringify(e.appointmentData.text);
-  schedule_name = schedule_name.substring(1, schedule_name.length - 1);
+  if (schedule_name == undefined) return 
+  else schedule_name = schedule_name.substring(1, schedule_name.length - 1);
   var all_day = e.appointmentData.allDay
   var recurrence_rule = JSON.stringify(e.appointmentData.recurrenceRule)
-  recurrence_rule = recurrence_rule.substring(1, recurrence_rule.length -1)
-  console.log(meta_data)
+  if (recurrence_rule == undefined) recurrence_rule = null;
+  else recurrence_rule = recurrence_rule.substring(1, recurrence_rule.length -1)
+  console.log(schedule_name, start_time, end_time, description, all_day, recurrence_rule, meta_data)
   addSchedule(schedule_name, start_time, end_time, description, all_day, recurrence_rule, meta_data)
 };
 
 const onAppointmentUpdating = (e) => {
-  fetchSchedule();
+  //fetchSchedule();
   //console.log(e)
   // var words = JSON.stringify(e.appointmentData.startDate)
   // var date = words.substring(1, 11)
